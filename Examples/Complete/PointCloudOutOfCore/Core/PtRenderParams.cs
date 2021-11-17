24b34434db2234dd68b59011d47ee9794e86827d
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2021-11-17 10:04:56 +0100

    removed unused local variables to fix app crashing on startup

--- Examples/Complete/PointCloudOutOfCore/Core/PtRenderParams.cs
+++ Examples/Complete/PointCloudOutOfCore/Core/PtRenderParams.cs
@@ -15,7 +15,7 @@ namespace Fusee.Examples.PointCloudOutOfCore.Core
 
         public ConcurrentDictionary<int, object> ShaderParamsToUpdate = new();
         public int MaxNoOfVisiblePoints = 500000;
-        public string PathToOocFile = "G://Studium//Semester 4//MuVista//Testcloud";
+        public string PathToOocFile = "G://Studium//Semester 4//MuVista//Cloud";
 
         public ShaderEffect DepthPassEf;
         public ShaderEffect ColorPassEf;
